52e7f0722e970b14f81aeddae28f87d8d9785104
joesty <gustavo.joesty@live.com>
2022-06-26 00:27:14 -0300

    Commit 9

--- Rental.java
+++ Rental.java
@@ -1,44 +1,26 @@
 public class Rental {
 
-   private Movie _movie;
-   private int _daysRented;
+    private Movie _movie;
+    private int _daysRented;
 
-   public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-   }
-
-   public int getDaysRented() {
-      return _daysRented;
-   }
-
-   public Movie getMovie() {
-      return _movie;
-   }
-   public int getFrequentRenterPoints() { 
-      //Adicionar o trecho de código extraído.
-      return 1;
-   }
-
-   public double getCharge() {
-    //Adicionar o trecho de código extraído.
-    //determine amounts for each line
-    double thisAmount = 0;
-    switch (getMovie().getPriceCode()) {
-       case Movie.REGULAR:
-          thisAmount += 2;
-          if (getDaysRented() > 2)
-             thisAmount += (getDaysRented() - 2) * 1.5;
-          break;
-       case Movie.NEW_RELEASE:
-          thisAmount += getDaysRented() * 3;
-          break;
-       case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (getDaysRented() > 3)
-             thisAmount += (getDaysRented() - 3) * 1.5;
-           break;
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
     }
-    return thisAmount;
-  }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+    public int getFrequentRenterPoints() { 
+        //Adicionar o trecho de código extraído.
+        return 1;
+    }
+
+    public double getCharge(){
+        return _movie.getCharge(_daysRented);
+    }
 }
