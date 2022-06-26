f389d6c6058acb7be2947d75e3e421139de723c0
joesty <gustavo.joesty@live.com>
2022-06-26 01:08:39 -0300

    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -3,6 +3,9 @@ public class NewReleasePrice extends Price {
         return Movie.NEW_RELEASE;
     }
     public double getCharge(int daysRented){
-      return daysRented * 3;
-   } 
+         return daysRented * 3;
+    }
+    public int getFrequentRenterPoints(int daysRented) {
+       return (daysRented > 1) ? 2: 1;
+    }
 }
