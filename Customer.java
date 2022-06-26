153385fdf17a4a4233b0e7e881d00c45ea3bb912
joesty <gustavo.joesty@live.com>
2022-06-25 23:25:07 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -5,6 +5,28 @@ public class Customer {
    private String _name;
    private Vector _rentals = new Vector();
 
+  private double amountFor(Rental each) {
+    //Adicionar o trecho de código extraído.
+    //determine amounts for each line
+    double thisAmount = 0;
+    switch (each.getMovie().getPriceCode()) {
+       case Movie.REGULAR:
+          thisAmount += 2;
+          if (each.getDaysRented() > 2)
+             thisAmount += (each.getDaysRented() - 2) * 1.5;
+          break;
+       case Movie.NEW_RELEASE:
+          thisAmount += each.getDaysRented() * 3;
+          break;
+       case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (each.getDaysRented() > 3)
+             thisAmount += (each.getDaysRented() - 3) * 1.5;
+           break;
+    }
+    return thisAmount;
+  }
+
    public Customer (String name){
       _name = name;
    }
@@ -25,23 +47,7 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -62,3 +68,4 @@ public class Customer {
      return result;
    }
 }
+
